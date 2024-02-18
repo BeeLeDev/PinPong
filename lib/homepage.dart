14ed9756ff0d79d91af3194b097b02ccac1c7af7
oopsuwu <60373320+oopsuwu@users.noreply.github.com>
2024-02-18 10:47:07 -0500

    add daily quest

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -22,15 +22,10 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               widget.title,
               style: const TextStyle(
-                color: Colors.white,
+                color: Color.fromARGB(255, 0, 0, 0),
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
               ),
-            ),
-            const Expanded(child: SizedBox()),
-            Image.asset(
-              'assets/sign_out.png',
-              scale: 2.5,
             ),
           ],
         ),
@@ -39,90 +34,282 @@ class _MyHomePageState extends State<MyHomePage> {
       body:
           // Cards Display
           Column(
-        children: [
-          SizedBox(
-            height: 150,
-            child: InkWell(
-              onTap: () {
-                // Add the navigation code here
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const GameLobbyPage()),
-                );
-              },
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius:
-                      BorderRadius.circular(35.0), // Adjust the value as needed
-                ),
-                color: const Color.fromRGBO(255, 77, 77, 1),
-                child: Row(
-                  children: [
-                    Container(
-                      width: 150,
-                      height: 110,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          image: AssetImage('assets/cc.jpg'),
-                          fit: BoxFit.cover,
-                          alignment: Alignment(.2, 0),
+            children: [
+            // Trivia Card
+            SizedBox(
+              height: 150,
+              child: InkWell(
+                onTap: () {
+                  // Add the navigation code here
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const GameLobbyPage()),
+                  );
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(35.0), // Adjust the value as needed
+                  ),
+                  color: const Color.fromRGBO(255, 77, 77, 1),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 110,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage('assets/cc.jpg'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment(.2, 0),
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      child: Container(),
-                    ),
-                    const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Trivia",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                          ),
-                        ),
-                        Text(
-                          "Campus Center",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
-                        ),
-                        Text(
-                          "14m until the next game",
-                          style: TextStyle(
-                            color: Color.fromRGBO(254, 213, 67, 1),
-                            fontSize: 20,
-                          ),
-                        ),
-                        Row(children: [
-                          Image(
-                            image: AssetImage('assets/person_icon.png'),
-                          ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
                           Text(
-                            "16 players waiting...",
+                            "Trivia",
                             style: TextStyle(
                               color: Colors.white,
-                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
                             ),
                           ),
-                        ]),
-                      ],
-                    ),
-                    Container(
-                      width: 4,
-                    )
-                  ],
+                          Text(
+                            "Campus Center",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "14m until the next game",
+                            style: TextStyle(
+                              color: Color.fromRGBO(254, 213, 67, 1),
+                              fontSize: 20,
+                            ),
+                          ),
+                          Row(children: [
+                            Image(
+                              image: AssetImage('assets/person_icon.png'),
+                            ),
+                            Text(
+                              "16 players waiting...",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ]),
+                        ],
+                      ),
+                      Container(
+                        width: 4,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+
+            // Treasure Hunting
+            SizedBox(
+              height: 150,
+              child: InkWell(
+                onTap: () {
+                  // Add the navigation code here
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const GameLobbyPage()),
+                  );
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(35.0), // Adjust the value as needed
+                  ),
+                  color: const Color.fromRGBO(102, 77, 225, 1),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 110,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage('assets/wheatley.png'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment(.2, 0),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Treasure Hunt",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                          Text(
+                            "Wheatley",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "2m until the next game",
+                            style: TextStyle(
+                              color: Color.fromRGBO(254, 213, 67, 1),
+                              fontSize: 20,
+                            ),
+                          ),
+                          Row(children: [
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: "You are ",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: "52 ft",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: " away from this pin.",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+
+                          ]),
+                        ],
+                      ),
+                      Container(
+                        width: 4,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            // Daily Quest
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Daily Quest",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+
+            // Quest Card
+            SizedBox(
+              height: 150,
+              child: InkWell(
+                onTap: () {
+                  // Add the navigation code here
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const GameLobbyPage()),
+                  );
+                },
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(35.0), // Adjust the value as needed
+                  ),
+                  color: const Color.fromRGBO(77, 180, 255, 1),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 110,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage('assets/chowoi.png'),
+                            fit: BoxFit.cover,
+                            alignment: Alignment(.2, 0),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(),
+                      ),
+                      const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Find Bak Choi",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                            ),
+                          ),
+                          Text(
+                            "Challenge: Fist Fight",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "2 hours remaining",
+                            style: TextStyle(
+                              color: Color.fromRGBO(254, 213, 67, 1),
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: 4,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
       ),
     );
   }
